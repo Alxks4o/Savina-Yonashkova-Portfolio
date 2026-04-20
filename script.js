c6e369353139deb577574178e80db450162da81f
Your Name <yonashkovaleks07@gmail.com>
2026-04-20 21:44:09 +0100

    added cards to main, added animated sliding banner

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ function slowScroll(event, id) {
     block: "start"
   });
 }
+
+
+// Animated banner word duplication
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.querySelectorAll(".group").forEach(group => {
+
+        const original = group.innerHTML;
+
+        // repeat until wide enough for smooth scroll
+        while (group.scrollWidth < window.innerWidth) {
+            group.innerHTML += original;
+        }
+
+    });
+
+});
